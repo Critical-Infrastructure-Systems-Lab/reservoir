badfce50cb914148a9a6a655a010178e8d37f112
swd-turner <swd.turner@gmail.com>
2015-12-08 15:33:44 +0800

    GRanD databased added.

--- R/Data.R
+++ R/Data.R
@@ -2,8 +2,20 @@
 #'
 #' @name ResX_inflow.ts
 #' @docType data
-#' @author Sean Turner
 #' @format time series object
 #' @source \url{www.bom.gov.au/water/hrs/}
 #' @examples plot(ResX_inflow.ts)
+#' 
+#' 
+
+NULL
+
+#' GRanD database
+#' 
+#' @name GRanD
+#' @docType data
+#' @format data frame object
+#' @source \url{http://atlas.gwsp.org}
+#' @references Lehner, B., R-Liermann, C., Revenga, C., Vorosmarty, C., Fekete, B., Crouzet, P., Doll, P. et al.: High resolution mapping of the world's reservoirs and dams for sustainable river flow management. Frontiers in Ecology and the Environment. Source: GWSP Digital Water Atlas (2008). Map 81: GRanD Database (V1.0).
+
 NULL
